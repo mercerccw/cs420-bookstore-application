21700a01b195e1f393405255ce1565a0c62d9007
Clayton Mercer <mercerccw@gmail.com>
2020-02-19 13:13:07 -0500

    Initial commit

---
+++ src/main/java/dao/JDBC.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+
+import javax.inject.Qualifier;
+
+@Qualifier @Retention(RetentionPolicy.RUNTIME) @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+public @interface JDBC {}
---
+++ src/test/java/dao/BookDAOJSONImplTest.java
@@ -0,0 +1,14 @@
+package dao;
+
+// Uncomment for use
+
+// import org.junit.jupiter.api.*;
+// import static org.junit.jupiter.api.Assertions.*;
+
+public class BookDAOJSONImplTest {
+
+	// @Test
+	// myTestMethod(){
+	//    assertEquals(1,1);
+	// }
+}
